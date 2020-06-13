ec4e10c3c4dd1a7842f05f097c3895b503bf9eb6
admin <admin@MacBook-Pro-69.local>
2020-06-13 13:57:44 -0400

    Deleted comments

--- project/app/src/main/java/com/example/skhool/MainActivity.java
+++ project/app/src/main/java/com/example/skhool/MainActivity.java
@@ -10,10 +10,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        //Hello Worldp
-
-        // This is Mannendri's comment
-        // This is Mannendri's second comment
     }
 }
