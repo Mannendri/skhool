6be8c138ca28e1f0fecd6f6b3c83cc97d71a0eb3
admin <admin@MacBook-Pro-69.local>
2020-06-13 14:32:54 -0400

    Worked on XML for home page

--- project/app/src/main/java/com/example/skhool/MainActivity.java
+++ project/app/src/main/java/com/example/skhool/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.skhool;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openNavigationActivity(View view) {
+        Intent intentToOpenNavigationActivity = new Intent(this,);
+        startActivity(intentToOpenNavigationActivity);
     }
 }
