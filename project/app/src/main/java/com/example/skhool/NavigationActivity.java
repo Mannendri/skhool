d4da1d43d6e71a42ee8e13f07346f4a9e963a1a7
admin <admin@MacBook-Pro-69.local>
2020-06-20 01:02:58 -0400

    Connected to Firebase

--- project/app/src/main/java/com/example/skhool/NavigationActivity.java
+++ project/app/src/main/java/com/example/skhool/NavigationActivity.java
@@ -6,12 +6,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class NavigationActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigation);
+// Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
     }
 
     public void openResourceCenterActivity(View view) {
